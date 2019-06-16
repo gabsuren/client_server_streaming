a2e3161d9e71c6ff36bff0bc2365041320c275c3
Suren <gabrielyansuren@gmail.com>
2019-06-17 01:10:50 +0400

    Added doxygen comments

--- server/server.cpp
+++ server/server.cpp
@@ -1,3 +1,13 @@
+/** @file server.cpp
+ *  @brief This file contains http streaming server implementation
+ *
+ *  This contains the prototypes for the console
+ *  driver and eventually any macros, constants,
+ *  or global variables you will need.
+ *
+ *  @author Suren G.
+ */
+
 #include <memory>
 #include <cstdint>
 #include <iostream>
@@ -6,16 +16,40 @@
 
 #define USERNAME "TEST"
 #define PASSWORD "PASS"
-#define DUMMY_TOKEN "7yn9160zQZuVjtdiGnUH4VGanAFgNyNn"
+#define DUMMY_TOKEN "7yn9160zQZuVjtdiGnUH4VGanAFgNyNn" // Dummy token, now hard coded, but should be generated each time connection estableshed or session expired.
 
+/** @brief Calls when there is generic http request
+ *  @param req http request
+ *  @return Void.
+ */
 void gen_req_cb(evhttp_request *req, void *);
-void login_req_cb(evhttp_request *req, void *);
-void frames_req_cb(evhttp_request *req, void *);
-void print_bytes(char *str, void *buffer, size_t len, char *type);
-char *generate_token(char *str);
 
-char *srv_address = "127.0.0.1";
-uint16_t srv_port = 5555;
+/** @brief Calls when there is login request
+ *  @param req http request
+ *  @return Void.
+ */
+void login_req_cb(evhttp_request *req, void *);
+
+/** @brief Calls when there is frame request
+ *  @param req http request
+ *  @return Void.
+ */
+void frames_req_cb(evhttp_request *req, void *);
+
+/** @brief Prints buffer as row data
+ *  @param str Title of printed data
+ *  @param buffer Data which should be printed
+ *  @param len Length of the buffer
+ *  @param type of data
+ *  @return Void.
+ */
+void print_bytes(char *str, void *buffer, size_t len, char *type);
+
+char *generate_token(char *str);
+bool verify_token(evhttp_request *req);
+
+char *srv_address = "127.0.0.1"; // Server's address
+uint16_t srv_port = 5555; // Server's port
 
 
 int main()
@@ -31,6 +65,7 @@ int main()
         fprintf(stderr, "Failed to init http server.");
         return -1;
     }
+  
     evhttp_set_gencb(server.get(), &gen_req_cb, nullptr);
     evhttp_set_cb(server.get(), "/login", &login_req_cb, NULL);
     evhttp_set_cb(server.get(), "/frames", &frames_req_cb, NULL);
@@ -56,29 +91,20 @@ void login_req_cb(evhttp_request *req, void *)
     struct evbuffer *input_buf = evhttp_request_get_input_buffer(req);
     if (!input_buf)
     {
+        evhttp_send_reply(req, 401, "", out_buf);
         return;
     }
     
     struct evbuffer *out_buf = evhttp_request_get_output_buffer(req); 
     size_t input_len = evbuffer_get_length(evhttp_request_get_input_buffer(req)); 
-    // size_t output_len = evbuffer_get_length(evhttp_request_get_output_buffer(req)); 
     char data[input_len + 1];
     data[input_len] = 0;
 
-    printf("Bytes read from input %d: \n", input_len);
     // copy POST body into your char array
     evbuffer_copyout(input_buf, data, input_len);
 
-    printf("Data: %s \n", data);
-    char *username = strtok(data, ":");
-    if( NULL == username) 
-    {
-        return;
-    }
-    char *password = strtok(NULL, "\0"); // Split string from last find delimiter
+    char *password = strtok(NULL, "\0"); // Split string from last find delimiter, strtok remembers last string
 
-    printf("username: %s \n", username);
-    printf("password: %s \n", password);
     if (strcmp(username, USERNAME) == 0 && strcmp(password, PASSWORD) == 0)
     {
         evbuffer_add_printf(out_buf, DUMMY_TOKEN);
@@ -92,12 +118,30 @@ void login_req_cb(evhttp_request *req, void *)
 
 void frames_req_cb(evhttp_request *req, void *)
 {
+    struct evbuffer *input_buf = evhttp_request_get_input_buffer(req);
+    if (!input_buf)
+    {
+        evhttp_send_reply(req, 401, "", out_buf);
+        return;
+    }
+
     auto *out_buf = evhttp_request_get_output_buffer(req);
     if (!out_buf)
         return;
     evbuffer_add_printf(out_buf, "<html><body><center><h1>Login!</h1></center></body></html>");
     evhttp_send_reply(req, HTTP_OK, "", out_buf);
 };
+
+bool verify_token(evhttp_request *req) 
+{
+    struct evbuffer *out_buf = evhttp_request_get_output_buffer(req); 
+    size_t input_len = evbuffer_get_length(evhttp_request_get_input_buffer(req)); 
+    char data[input_len + 1];
+    data[input_len] = 0;
+    if(strcmp(data, DUMMY_TOKEN) != 0)
+        return false;
+    return true;
+}
 
 void print_bytes(char *str, void *buffer, size_t len, char *type)
 {
